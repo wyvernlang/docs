33e5c73b465c3b43161ea9dab8a37437e2eea490
Julian Mackay <julian@bats.ecs.vuw.ac.nz>
2018-08-08 17:16:49 +1200

    type members: redefinition of shift/jump/subst

--- type_members/ftfjp2018/coq/vars.v
+++ type_members/ftfjp2018/coq/vars.v
@@ -27,16 +27,10 @@ Inductive label : Type :=
 
 Inductive ty : Type := (*types*)
 | str : decl_tys -> ty
-| sel : path -> label -> ty
-| fn_t  : ty -> var -> ty -> ty
+| sel : exp -> label -> ty
+| fn_t  : ty -> ty -> ty
 | top   : ty
 | bot  : ty
-
-with
-path : Type := (*paths*)
-| p_loc : nat -> path
-| p_var : var -> path
-| p_cast : path -> ty -> path
                            
 with
 decl_ty : Type := (*declaration types*)
@@ -47,23 +41,17 @@ decl_ty : Type := (*declaration types*)
 with
 decl_tys : Type :=
 | dt_nil : decl_tys
-| dt_con : decl_ty -> decl_tys -> decl_tys.
+| dt_con : decl_ty -> decl_tys -> decl_tys
 
-
-Inductive exp : Type := (*expressions*)
+with
+exp : Type := (*expressions*)
 | new : decls -> exp
-| app : exp -> exp -> exp
-| e_fn : nat -> ty -> exp -> exp
+| e_app : exp -> exp -> exp
+| e_fn : ty -> exp -> ty -> exp
 | e_acc : exp -> label -> exp
 | e_var : var -> exp
 | e_loc : nat -> exp
 | e_cast : exp -> ty -> exp
-                                       
-(*with
-path : Type := (*paths*)
-| p_var : var -> path
-| p_loc : nat -> path
-| p_cast : path -> ty -> path*)
 
 with
 decl : Type := (*declarations*)
@@ -77,7 +65,6 @@ decls : Type :=
 | d_con : decl -> decls -> decls.
 
 
-
 (*Notation "p 'cast' t" := (p_cast p t) (at level 80).
 Notation "'v_' x" := (p_var x) (at level 80).*)
 
@@ -89,13 +76,16 @@ Notation "e 'cast' t" := (e_cast e t) (at level 80).
 
 Notation "'type' l 'sup' t" := (dt_low l t) (at level 80).
 Notation "'type' l 'ext' t" := (dt_upp l t) (at level 80).
-Notation "'fn' n 'off' t 'in_exp' e" := (e_fn n t e) (at level 80).
-Notation "'val' l 'ofv' t" := (dt_val l t) (at level 80).
+Notation "'type' l 'supe' t" := (d_low l t) (at level 80).
+Notation "'type' l 'exte' t" := (d_upp l t) (at level 80).
+Notation "'val' l 'oft' t" := (dt_val l t) (at level 80).
+Notation "'val' l 'assgn' e" := (d_val l e) (at level 80).
+Notation "'fn' t1 'in_exp' e 'off' t2" := (e_fn t1 e t2) (at level 80).
 Notation "t1 'arr' t2" := (fn_t t1 t2) (at level 80).
 
 Inductive is_value : exp -> Prop :=
 | v_loc : forall i, is_value (i_ i)
-| v_fn : forall x t e, is_value (fn x off t in_exp e)
+| v_fn : forall t1 e t2, is_value (fn t1 in_exp e off t2)
 | v_cast : forall v t, is_value v ->
                   is_value (v cast t).
 
@@ -108,15 +98,17 @@ Inductive is_path : exp -> Prop :=
 Scheme type_mut_ind := Induction for ty Sort Prop
   with decl_ty_mut_ind := Induction for decl_ty Sort Prop
   with decl_tys_mut_ind := Induction for decl_tys Sort Prop
-  (*with path_mut_ind := Induction for path Sort Prop*).
+  with exp_mut_ind := Induction for exp Sort Prop
+  with decl_mut_ind := Induction for decl Sort Prop
+  with decls_mut_ind := Induction for decls Sort Prop.
 
-Combined Scheme type_mutind from type_mut_ind, decl_ty_mut_ind, decl_tys_mut_ind.
+Combined Scheme type_exp_mutind from type_mut_ind, decl_ty_mut_ind, decl_tys_mut_ind, exp_mut_ind, decl_mut_ind, decls_mut_ind.
 
 Definition id (d : decl_ty) : label :=
   match d with
   | type l sup _ => l
   | type l ext _ => l
-  | val l ofv _ => l
+  | val l oft _ => l
   end.
 
 
@@ -128,64 +120,6 @@ Section variables.
   Reserved Notation "'[' p '/d' n ']' d" (at level 80).
   Reserved Notation "'[' p1 '/p' n ']' p2" (at level 80).
 
-  Definition switch_n (n1 n2 m : nat) : nat :=
-    if beq_nat n1 m
-    then n2
-    else if beq_nat n2 m
-         then n1
-         else m.
-
-  Notation "'[' n1 'swap_n' n2 ']' m" := (switch_n n1 n2 m) (at level 80).
-
-  Definition switch_v (n m : nat) (x : var) : var :=
-    match x with
-      | Var n' => Var ([n swap_n m] n')
-      | _ => x
-    end.
-  
-  Notation "'[' n 'swap_v' m ']'" := (switch_v n m) (at level 80).
-
-  Fixpoint switch (n m : nat) (t : ty) : ty :=
-    match t with
-    | top => top
-    | bot => bot
-    | t1 arr t2 => (switch n m t1) arr (switch (S n) (S m) t2) 
-    | sel x l => sel (switch_v n m x) l
-    | str ds => str (switch_ds (S n) (S m) ds)
-    end
-
-  (*with
-  switch_p (n m : nat) (p : path) : path :=
-    match p with
-    | v_ x => v_ [n swap_v m] x
-    | p' cast t => (switch_p n m p') cast (switch n m t)
-    end*)
-
-  with
-  switch_d (n m : nat) (d : decl_ty) : decl_ty :=
-    match d with
-    | type l sup t => type l sup (switch n m t)
-    | type l ext t => type l ext (switch n m t)
-    | val l ofv t => val l ofv (switch n m t)
-    end
-
-  with
-  switch_ds (n m : nat) (ds : decl_tys) : decl_tys :=
-    match ds with
-      | dt_nil => dt_nil
-      | dt_con d ds' => dt_con (switch_d n m d) (switch_ds n m ds')
-    end.
-
-  Notation "'[' n 'swap' m ']'" := (switch n m) (at level 80).
-(*  Notation "'[' n 'swap_p' m ']'" := (switch_p n m) (at level 80).*)
-  Notation "'[' n 'swap_d' m ']'" := (switch_d n m) (at level 80).
-  
-
-  Definition switch_env (n m : nat) (G : env) : env :=
-    map ([n swap m]) G.
-  
-  Notation "'[' n 'swap_e' m ']'" := (switch_env n m) (at level 80).
-
   (*Left Shift*)
   
   Fixpoint left_shift_var (n : nat) (v : var) : var :=
@@ -196,11 +130,11 @@ Section variables.
       
   Notation "v 'lshift_v' n" := (left_shift_var n v) (at level 80).
   Reserved Notation "t 'lshift_t' n" (at level 80).
-  Reserved Notation "d 'lshift_d' n" (at level 80).
-  Reserved Notation "d 'lshift_ds' n" (at level 80).
   Reserved Notation "d 'lshift_dt' n" (at level 80).
   Reserved Notation "d 'lshift_dts' n" (at level 80).
-  Reserved Notation "p 'lshift_p' n" (at level 80).
+  Reserved Notation "e 'lshift_e' n" (at level 80).
+  Reserved Notation "d 'lshift_d' n" (at level 80).
+  Reserved Notation "d 'lshift_ds' n" (at level 80).
   Reserved Notation "G 'lshift_e' n" (at level 80).
   
   Fixpoint left_shift_type (n : nat) (t : ty) : ty :=
@@ -208,7 +142,7 @@ Section variables.
       | top => top
       | bot => bot
       | t1 arr t2 => (t1 lshift_t n) arr (t2 lshift_t n)
-      | sel x l => sel (x lshift_v n) l
+      | sel p l => sel (p lshift_e n) l
       | str ds => str (ds lshift_dts n)
     end
       where "t 'lshift_t' n" := (left_shift_type n t)
@@ -218,7 +152,7 @@ Section variables.
     match d with
       | type l sup t => type l sup (t lshift_t n)
       | type l ext t => type l ext (t lshift_t n)
-      | val l ofv t => val l ofv (t lshift_t n)
+      | val l oft t => val l oft (t lshift_t n)
     end
       where "d 'lshift_dt' n" := (left_shift_decl_ty n d)
 
@@ -230,13 +164,35 @@ Section variables.
     end
       where "d 'lshift_dts' n" := (left_shift_decl_tys n d)
 
-  (*with
-  left_shift_path (n : nat) (p : path) : path :=
-    match p with
-      | v_ x => v_ (x lshift_v n)
-      | p cast t => (p lshift_p n) cast (t lshift_t n)
+  with
+  left_shift_exp (n : nat) (e : exp) : exp :=
+    match e with
+    | new ds => new (ds lshift_ds S n)
+    | e_app e1 e2 => e_app (e1 lshift_e n) (e2 lshift_e n)
+    | fn t1 in_exp e off t2 => fn (t1 lshift_t n) in_exp (e lshift_e S n) off (t2 lshift_t S n)
+    | e_acc e m => e_acc (e lshift_e n) m
+    | e cast t => (e lshift_e n) cast (t lshift_t n)
+    | v_ x => v_ (x lshift_v n)
+    | i_ i => i_ i
     end
-      where "p 'lshift_p' n" := (left_shift_path n p)*).
+      where "e 'lshift_e' n" := (left_shift_exp n e)
+
+  with
+  left_shift_decl (n : nat) (d : decl) : decl :=
+    match d with
+      | type l supe t => type l supe (t lshift_t n)
+      | type l exte t => type l exte (t lshift_t n)
+      | val l assgn e => val l assgn (e lshift_e n)
+    end
+      where "d 'lshift_d' n" := (left_shift_decl n d)
+
+  with
+  left_shift_decls (n : nat) (ds : decls) : decls :=
+    match ds with
+      | d_nil => d_nil
+      | d_con d ds' => d_con (d lshift_d n) (ds' lshift_ds n)
+    end
+      where "d 'lshift_ds' n" := (left_shift_decls n d).
 
   
 
@@ -263,7 +219,7 @@ Section variables.
     | top => top
     | bot => bot
     | t1 arr t2 => (t1 rshift_t n) arr (t2 rshift_t n)
-    | sel x l => sel (x rshift_v n) l
+    | sel p l => sel (p rshift_e n) l
     | str ds => str (ds rshift_dts n)
     end
   where "t 'rshift_t' n" := (right_shift_type n t)
@@ -273,7 +229,7 @@ Section variables.
     match d with
     | type l sup t => type l sup (t rshift_t n)
     | type l ext t => type l ext (t rshift_t n)
-    | val l ofv t => val l ofv (t rshift_t n)
+    | val l oft t => val l oft (t rshift_t n)
     end
   where "d 'rshift_dt' n" := (right_shift_decl_ty n d)
 
@@ -284,6 +240,37 @@ Section variables.
       | dt_con d ds' => dt_con (d rshift_dt n) (ds' rshift_dts n)
     end
   where "d 'rshift_dts' n" := (right_shift_decl_tys n d)
+                                
+  with
+  right_shift_exp (n : nat) (e : exp) : exp :=
+    match e with
+    | new ds => new (ds rshift_ds S n)
+    | e_app e1 e2 => e_app (e1 rshift_e n) (e2 rshift_e n)
+    | fn t1 in_exp e off t2 => fn (t1 rshift_t n) in_exp (e rshift_e S n) off (t2 rshift_t S n)
+    | e_acc e m => e_acc (e rshift_e n) m
+    | e cast t => (e rshift_e n) cast (t rshift_t n)
+    | v_ x => v_ (x rshift_v n)
+    | i_ i => i_ i
+    end
+      where "e 'rshift_e' n" := (right_shift_exp n e)
+
+  with
+  right_shift_decl (n : nat) (d : decl) : decl :=
+    match d with
+      | type l supe t => type l supe (t rshift_t n)
+      | type l exte t => type l exte (t rshift_t n)
+      | val l assgn e => val l assgn (e rshift_e n)
+    end
+      where "d 'rshift_d' n" := (right_shift_decl n d)
+
+  with
+  right_shift_decls (n : nat) (ds : decls) : decls :=
+    match ds with
+      | d_nil => d_nil
+      | d_con d ds' => d_con (d rshift_d n) (ds' rshift_ds n)
+    end
+      where "d 'rshift_ds' n" := (right_shift_decls n d).
+
 
   (*with
   right_shift_path (n : nat) (p : path) : path :=
@@ -291,7 +278,7 @@ Section variables.
     | v_ x => v_ (x rshift_v n)
     | p cast t => (p rshift_p n) cast (t rshift_t n)
     end
-      where "p 'rshift_p' n" := (right_shift_path n p)*).
+      where "p 'rshift_p' n" := (right_shift_path n p)*)
 
   (*Environment shift*)
   
@@ -356,7 +343,7 @@ in G1 that refer to positions in G1 do not change, and similarly, all references
       | top => top
       | bot => bot
       | t1 arr t2 => (t1 [i] ljump_t n) arr (t2 [inc i 1] ljump_t n)
-      | sel x l => sel (x [i] ljump_v n) l
+      | sel p l => sel (p [i] ljump_e n) l
       | str ds => str (ds [inc i 1] ljump_dts n)
     end
       where "t '[' i ']' 'ljump_t' n" := (left_jump_t t n i)
@@ -366,7 +353,7 @@ in G1 that refer to positions in G1 do not change, and similarly, all references
     match d with
       | type l sup t => type l sup (t[i] ljump_t n)
       | type l ext t => type l ext (t[i] ljump_t n)
-      | val l ofv t => val l ofv (t[i] ljump_t n)
+      | val l oft t => val l oft (t[i] ljump_t n)
     end
       where "d '[' i ']' 'ljump_dt' n" := (left_jump_d_ty d n i)
 
@@ -378,27 +365,42 @@ in G1 that refer to positions in G1 do not change, and similarly, all references
     end
       where "d '[' i ']' 'ljump_dts' n" := (left_jump_d_tys d n i)
 
-  (*with
-  left_jump_p (p : path) (n : nat) (i : option nat) : path :=
-    match p with
-      | v_ x => v_ (x [i] ljump_v n)
-      | p cast t => (p [i] ljump_p n) cast (t [i] ljump_t n)
+  with
+  left_jump_e (e : exp) (n : nat) (i : option nat) : exp :=
+    match e with
+    | new ds => new (ds [inc i 1] ljump_ds n)
+    | e_app e1 e2 => e_app (e1 [i] ljump_e n) (e2 [i] ljump_e n)
+    | fn t1 in_exp e off t2 => fn (t1 [i] ljump_t n) in_exp (e [inc i 1] ljump_e n) off (t2 [inc i 1] ljump_t n)
+    | e_acc e m => e_acc (e [i] ljump_e n) m
+    | v_ x => v_ (x [i] ljump_v n)
+    | i_ i => i_ i
+    | e cast t => (e [i] ljump_e n) cast (t [i] ljump_t n)
     end
-  where "p '[' i ']' 'ljump_p' n" := (left_jump_p p n i)*).
+      where "e '[' i ']' 'ljump_e' n" := (left_jump_e e n i)        
+                                             
+  with
+  left_jump_d (d : decl) (n : nat) (i : option nat) : decl :=
+    match d with
+      | type l supe t => type l supe (t[i] ljump_t n)
+      | type l exte t => type l exte (t[i] ljump_t n)
+      | val l assgn e => val l assgn (e[i] ljump_e n)
+    end
+      where "d '[' i ']' 'ljump_d' n" := (left_jump_d d n i)
 
-(*  Definition left_jump_option (i n : nat) (o : option ty) : option ty :=
-    match o with
-    | None => None
-    | Some t => Some (t [i] ljump_t n)
-    end.
-
-  Notation "o '[' i ']' 'ljump_o' n" := (left_jump_option i n o)(at level 80).*)
+  with
+  left_jump_ds (d : decls) (n : nat) (i : option nat) : decls :=
+    match d with
+      | d_nil => d_nil
+      | d_con d ds' => d_con (d [i] ljump_d n) (ds' [i] ljump_ds n)
+    end
+  where "d '[' i ']' 'ljump_ds' n" := (left_jump_ds d n i).
+  
   Reserved Notation "'[' p '/t' n ']' ds" (at level 80).
   Reserved Notation "'[' p '/dt' n ']' ds" (at level 80).
   Reserved Notation "'[' p '/dts' n ']' ds" (at level 80).
   Reserved Notation "'[' p '/d' n ']' ds" (at level 80).
   Reserved Notation "'[' p '/ds' n ']' ds" (at level 80).
-  Reserved Notation "'[' p '/p' n ']' ds" (at level 80).
+  Reserved Notation "'[' p '/e' n ']' ds" (at level 80).
 
 
   Definition subst_v (n : nat) (x y : var) : var :=
@@ -411,43 +413,81 @@ in G1 that refer to positions in G1 do not change, and similarly, all references
   
   Notation "'[' x '/v' n ']' y" := (subst_v n x y) (at level 80).
     
-  Fixpoint subst (n : nat) (x : var) (t : ty) : ty :=
+  Fixpoint subst (n : nat) (e : exp) (t : ty) : ty :=
     match t with
     | top => top
     | bot => bot
-    | t1 arr t2 => ([x /t n] t1) arr ([x lshift_v 1 /t S n] t2)
-    | sel y l => sel ([ x /v n ] y) l
-    | str ds => str ([ x lshift_v 1 /dts S n ] ds)
+    | t1 arr t2 => ([e /t n] t1) arr ([e lshift_e 1 /t S n] t2)
+    | sel p l => sel ([ e /e n ] p) l
+    | str ds => str ([ e lshift_e 1 /dts S n ] ds)
     end
 
   where "'[' p '/t' n ']' t" := (subst n p t)
 
   with
-  subst_d_ty (n : nat) (x : var) (d : decl_ty) : decl_ty :=
+  subst_d_ty (n : nat) (e : exp) (d : decl_ty) : decl_ty :=
     match d with
-    | type l sup t => type l sup ([x /t n] t)
-    | type l ext t => type l ext ([x /t n] t)
-    | val l ofv t => val l ofv ([x /t n] t)                        
+    | type l sup t => type l sup ([e /t n] t)
+    | type l ext t => type l ext ([e /t n] t)
+    | val l oft t => val l oft ([e /t n] t)                        
     end
       
   where "'[' p '/dt' n ']' d" := (subst_d_ty n p d)
 
   with
-  subst_d_tys (n : nat) (x : var) (d : decl_tys) : decl_tys :=
+  subst_d_tys (n : nat) (e : exp) (d : decl_tys) : decl_tys :=
     match d with
-      | dt_nil => dt_nil
-      | dt_con d ds' => dt_con ([x /dt n] d) ([x /dts n] ds')
+    | dt_nil => dt_nil
+    | dt_con d ds' => dt_con ([e /dt n] d) ([e /dts n] ds')
     end
       
-      where "'[' p '/dts' n ']' d" := (subst_d_tys n p d).
+  where "'[' p '/dts' n ']' d" := (subst_d_tys n p d)
 
-  Fixpoint subst_env (n : nat)(x : var)(G : env) : env :=
+  with
+  subst_e (n : nat) (e1 e2 : exp) : exp :=
+    match e2 with
+    | new ds => new ([e1 /ds n] ds)
+    | e_app e e' => e_app ([e1 /e n] e) ([e1 /e n] e')
+    | fn t1 in_exp e off t2 => fn ([e1 /t n] t1) in_exp ([e1 /e n] e) off ([e1 /t S n] t2)
+    | e_acc e m => e_acc ([e1 /e n] e) m
+    | v_ x => match x with
+             | Abs m => if beq_nat n m
+                       then e1
+                       else v_ x
+             | _ => v_ x
+             end
+    | i_ i => i_ i
+    | e cast t => ([e1 /e n] e) cast ([e1 /t n] t)
+    end
+      
+  where "'[' e1 '/e' n ']' e2" := (subst_e n e1 e2)
+
+  with
+  subst_d (n : nat) (e : exp) (d : decl) : decl :=
+    match d with
+    | type l exte t => type l exte ([e /t n] t)
+    | type l supe t => type l supe ([e /t n] t)
+    | val l assgn el => val l assgn ([e /e n] el)
+    end
+      
+  where "'[' p '/d' n ']' d" := (subst_d n p d)
+
+  with
+  subst_ds (n : nat) (e : exp) (d : decls) : decls :=
+    match d with
+    | d_nil => d_nil
+    | d_con d ds' => d_con ([e /d n] d) ([e /ds n] ds')
+    end
+      
+  where "'[' p '/ds' n ']' d" := (subst_ds n p d).
+
+  Fixpoint subst_env (n : nat)(e : exp)(G : env) : env :=
     match G with
-      | nil => nil
-      | t::G' => match n with
-                  | O => ([x /t n]t)::G'
-                  | S n' => ([x /t n]t)::(subst_env n' (x rshift_v 1) G')
-                end
+    | nil => nil
+    | t::G' => match n with
+              | O => ([e /t n]t)::G'
+              | S n' => ([e /t n]t)::(subst_env n' (e rshift_e 1) G')
+              end
     end.
 
   Notation "'[' x '/e' n ']' G" := (subst_env n x G)(at level 80).
@@ -484,7 +524,7 @@ in G1 that refer to positions in G1 do not change, and similarly, all references
   
   Reserved Notation "G 'vdash' p 'hasType' t" (at level 80).
   
-  Inductive typing : env -> var -> ty -> Prop :=
+  Inductive typing : env -> exp -> ty -> Prop :=
   | t_var : forall G n t, get n G = Some t ->
                      G vdash (c_ n) hasType (t lshift_t (S n))
   (*| t_cast : forall G p t, G vdash (p cast t) hasType t*)
